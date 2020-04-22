85ed14ea21609c5593bad29e3169cb563222dd07
Bryce Payne <brycedpayne@gmail.com>
2020-04-22 11:47:24 -0500

    practice

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/count")
 public class CounterServlet extends HttpServlet {
@@ -12,4 +13,30 @@ public class CounterServlet extends HttpServlet {
         counter += 1;
         response.getWriter().println("<h1>The count is " + counter + ".</h1>");
     }
+
+public static ArrayList<Integer> findFactors012(int num){
+        ArrayList<Integer> factors = new ArrayList<>();
+
+        for (int i = 1; i <= num; i++){
+            if (num%i==0){
+                factors.add(i);
+            }
+        }
+
+        return factors;
+}
+
+    public static void main(String[] args) {
+//        byte zero = 0;
+//        int one = 1;
+//        char letter = 'H';
+//        long lon = 3110;
+//        double dec = 2.0;
+//        boolean val = true;
+//
+//        String output = "" + letter + lon + " w" + zero + "r" + one + "d " + dec + " " +val;
+//        System.out.println(output);
+        System.out.println(findFactors012(144));
+    }
+
 }
--- src/main/webapp/pickcolor.jsp
+++ src/main/webapp/pickcolor.jsp
@@ -6,11 +6,33 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    String bgcolor;
+    if (request.getParameterMap().containsKey("color")) {
+        bgcolor = request.getParameter("color");
+    }
+    else {
+        bgcolor = "white";
+    }
+
+%>
 <html>
+
 <head>
     <title>Pick Color</title>
+    <style type="text/css">
+        body {background: <% bgcolor %>}
+    </style>
 </head>
 <body>
+
+<form action="/pickcolor.jsp" method="post">
+
+    <label for="color">Please enter a color:</label>
+    <input type="text" id="color">
+    <button type="submit">Submit</button>
+    <% response.sendRedirect("/pickcolor.jsp?color=" + bgcolor); %>
+</form>
 
 </body>
 </html>
