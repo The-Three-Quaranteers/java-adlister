ab55a9c997ab619d8dff3bbc03d9db551c1265f7
Bryce Payne <brycedpayne@gmail.com>
2020-04-28 10:13:37 -0500

    refactors

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -17,7 +18,8 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
-            request.getSession().setAttribute("user",username);
+            HttpSession session = request.getSession();
+            session.setAttribute("user",username);
             request.getRequestDispatcher("/profile.jsp").forward(request, response);
         } else {
             response.sendRedirect("/login");
