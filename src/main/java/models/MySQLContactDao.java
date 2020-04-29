5598ab4f5789364a75c7de236b42b75f8f1e77a8
Bryce Payne <brycedpayne@gmail.com>
2020-04-29 14:31:33 -0500

    end of lecture

--- src/main/java/models/MySQLContactDao.java
+++ src/main/java/models/MySQLContactDao.java
@@ -54,17 +54,81 @@ public class MySQLContactDao implements Contacts {
 
     @Override
     public long saveContact(Contact contact) {
-        return 0;
+
+        long newlyCreatedUserID = 0;
+
+        // INSERT INTO contacts (first_name, last_name, phone_number) VALUES ('firstName', 'lastName', 'phoneNumber');
+
+        String addContactQuery = String.format("INSERT INTO contacts (first_name, last_name, phone_number) VALUES('%s','%s','%s')",
+                contact.getFirstName(),
+                contact.getLastName(),
+                contact.getPhoneNumber()
+        );
+
+        try {
+            Statement stmt = conn.createStatement();
+            //now let's execute the statement to add to DB
+            stmt.executeUpdate(addContactQuery, Statement.RETURN_GENERATED_KEYS);
+
+            ResultSet ks = stmt.getGeneratedKeys();
+            if (ks.next()){
+                newlyCreatedUserID = ks.getLong(1); //save the MySQL row ID to variable
+            }
+
+            // check to see if the id was returned or its still at the default "0"
+            if (newlyCreatedUserID != 0) {
+                contact.setId(newlyCreatedUserID);
+            }
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return newlyCreatedUserID;
     }
 
     @Override
     public void deleteContactById(long id) {
+        //DELETE FROM contacts WHERE id = sentInID
 
+        String query = String.format("DELETE FROM contacts WHERE id = %d", id);
+
+        try {
+            Statement stmt = conn.createStatement();
+            boolean success = stmt.execute(query);
+            if(success){
+                System.out.println("Contact has been removed.");
+            } else {
+                System.out.println("Contact was not removed - check for syntax errors.");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Contact getContactById(long id) {
-        return null;
+
+        Contact returnContact = new Contact();
+        String query = String.format("SELECT * FROM contacts WHERE id = %d", id);
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            if (rs.next()){
+                returnContact.setId(id);
+                returnContact.setFirstName(rs.getString("first_name"));
+                returnContact.setLastName(rs.getString("last_name"));
+                returnContact.setPhoneNumber(rs.getString("phone_number"));
+            }else{
+                System.out.println("No matching user found");
+
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return returnContact;
     }
 }
 
